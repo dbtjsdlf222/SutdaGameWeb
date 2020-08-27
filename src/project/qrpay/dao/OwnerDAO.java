9f2332f30db11521dbfbc716fcbc7668cdeffe8a
TJ <TJ@DESKTOP-G4SIDO2>
2020-08-27 12:38:41 +0900

    update

--- src/project/qrpay/dao/OwnerDAO.java
+++ src/project/qrpay/dao/OwnerDAO.java
@@ -1,14 +1,17 @@
 package project.qrpay.dao;
 
-import java.util.Map;
-
 import org.apache.ibatis.annotations.Param;
 
+import project.qrpay.vo.MenuVO;
 import project.qrpay.vo.OwnerVO;
+import project.qrpay.vo.StoreVO;
 
 public interface OwnerDAO {
-	
+	public void join(OwnerVO ownerVO);			//회원가입
+	public void seleteOwner(int no);
+	public void updateOwner(OwnerVO ownerVO);
+	public void deleteOwner(int no);
+	public int selectID(@Param("id") String id);
 	public OwnerVO login(@Param("id")String id, @Param("pw")String pw);		//로그인
-	public void join(Map map);			//회원가입
-	
+
 } //interface OwnerDAO
