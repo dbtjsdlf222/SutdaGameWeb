9fe5df491ed4bd07b13354e9bc98da24c4cea955
유선일 <34783191+dbtjsdlf222@users.noreply.github.com>
2020-09-14 21:09:14 +0900

    OwnerVO

--- src/project/qrpay/vo/OwnerVO.java
+++ src/project/qrpay/vo/OwnerVO.java
@@ -1,9 +1,12 @@
 package project.qrpay.vo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
 public class OwnerVO {
+	
 //owner DB
    private int no;             	//가입번호
    private String name;         //이름
@@ -11,7 +14,6 @@ public class OwnerVO {
    private String id;           //아이디
    private String pw;           //비밀번호
    private String email;      	//이메일
-   private String email2;      	//이메일 뒤에
    private Date membershipDate;   //가입일
    private int bankAccount;   //은행 계좌
    private String bankName;   //은행 계좌
@@ -33,35 +35,39 @@ public class OwnerVO {
 		this.id = map.get("id");
 		this.pw = map.get("pw");
 		this.email = map.get("email");
+
 		this.membershipDate = new Date();
-		this.bankAccount = map.get("bankAccount");
+		SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try { this.membershipDate = transFormat.parse(map.get("membershipDate")); } 
+		catch (ParseException e) { e.printStackTrace(); }
+		
+		this.bankAccount = Integer.parseInt(map.get("bankAccount"));
 		this.bankName = map.get("bankName");
 		this.storeName = map.get("storeName");
 		this.type = map.get("type");
-		this.licenseNumber = map.get("licenseNumber");
+		this.licenseNumber = Integer.parseInt(map.get("licenseNumber"));
 		this.address = map.get("address");
 	}
+   
 //사장님 getters
    public int getNo() {return no;}
    public String getName() {return name;}
-   public int getPhone() {return phone;}
+   public String getPhone() { return phone; }
    public String getId() {return id;}
    public String getPw() {return pw;}
-   public String getEmail1() {return email1;}
-   public String getEmail2() {return email2;}
-   public int getMembershipDate() {return membershipDate;}
+   public String getEmail() {return email;}
+   public Date getMembershipDate() {return membershipDate;}
    public int getBankAccount() { return bankAccount; }
    public String getBankName() { return bankName; }
    
 //사장님 setters
    public void setNo(int no) {this.no = no;}
    public void setName(String name) {this.name = name;}
-   public void setPhone(int phone) {this.phone = phone;}
+   public void setPhone(String phone) {this.phone = phone;}
    public void setId(String id) {this.id = id;}
    public void setPw(String pw) {this.pw = pw;}
-   public void setEmail1(String email) {this.email1 = email;}
-   public void setEmail2(String email2) {this.email2 = email2;}
-   public void setMembershipDate(int membershipDate) {this.membershipDate = membershipDate;}
+   public void setEmail1(String email) {this.email = email;}
+   public void setMembershipDate(Date membershipDate) {this.membershipDate = membershipDate;}
    public void setBankAccount(int bankAccount) { this.bankAccount = bankAccount; }
    public void setBankName(String bankName) { this.bankName = bankName; }
    
@@ -84,4 +90,4 @@ public class OwnerVO {
    public void setTableCount(int tableCount) {this.tableCount = tableCount;}
 
    
-}//main
+} //main
