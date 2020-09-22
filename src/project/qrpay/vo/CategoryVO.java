fda04e7a51f5670234507d27e06bb3f7ffe67477
SUN <SUN@DESKTOP-ELP114I>
2020-09-22 18:18:58 +0900

    update

---
+++ src/project/qrpay/vo/CategoryVO.java
@@ -0,0 +1,24 @@
+package project.qrpay.vo;
+
+public class CategoryVO {
+	private int no;
+	private String name;
+	private int menuNo;
+	
+	public int getNo() { return no; }
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(int menuNo) {
+		this.menuNo = menuNo;
+	}
+}
