2305730d482f44d4d6ba5a39eb48eadfd595c320
Lee <Lee@DESKTOP-L3PKHMB>
2020-10-07 23:54:20 +0900

    메일보내기 업데이트

--- src/sutdaGame/web/controller/MainController.java
+++ src/sutdaGame/web/controller/MainController.java
@@ -1,8 +1,12 @@
 package sutdaGame.web.controller;
 
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +16,9 @@ import sutdaGame.web.vo.OwnerVO;
 
 @Controller @RequestMapping("/")
 public class MainController {
+	
+	@Autowired
+	JavaMailSender mailSender;
 	
 	@Autowired
 	OwnerService ownerService;
@@ -89,5 +96,37 @@ public class MainController {
 		public String customer(HttpSession session) {
 			return "mainpage/customer";
 		}	//customer
+		
+	//메일보내기
+	@RequestMapping(value = "mailSending")
+    public String mailSending(HttpServletRequest request) {
+		   
+	    String setfrom = "apdlvmf1562@gmail.com";
+	    String tomail = request.getParameter("tomail"); 		// 받는 사람 이메일
+	    String title = request.getParameter("title"); 			// 제목
+	    String content = request.getParameter("content"); 		// 내용
+	    try {  	
+	    	MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom); 					// 보내는사람 생략하면 정상작동을 안함
+	        messageHelper.setTo(tomail);						// 받는사람 이메일
+	        messageHelper.setSubject(title); 					// 메일제목은 생략이 가능하다
+	        messageHelper.setText(content); 					// 메일 내용
+	        
+	        mailSender.send(message);	        
+	         } catch (Exception e) {
+	            e.printStackTrace();
+	         }
+	    
+	    return "mailForm";
+	}
+	         
+	         // mailForm
+	         @RequestMapping(value = "/mail")
+	         public String mailForm() {
+	          
+	           return "mail";
+	         }  
 		
 } //MainController class 
